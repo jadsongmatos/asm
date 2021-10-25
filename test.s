51d5c9c2438b7e6b70e1cf32c7cfd7876c9cb3ba
slender1808 <jadson.g-matos@outlook.com>
2021-10-24 23:53:29 -0300

    power

--- test.s
+++ test.s
@@ -1,22 +1,66 @@
-.global main
-    .text
+# -----------------------------------------------------------------------------
+# A 64-bit command line application to compute x^y.
+#
+# Syntax: power x y
+# x and y are integers
+# -----------------------------------------------------------------------------
 
+        .global main
+
+        .text
 main:
-        push    %rdi                    # save registers that puts uses
-        push    %rsi
-        sub     $8, %rsp                # must align stack before call
+        push    %r12                    # save callee-save registers
+        push    %r13
+        push    %r14
+        # By pushing 3 registers our stack is already aligned for calls
 
-        mov     (%rsi), %rdi            # the argument string to display
-        call    puts                    # print it
+        cmp     $3, %rdi                # must have exactly two arguments
+        jne     error1
 
-        add     $8, %rsp                # restore %rsp to pre-aligned value
-        pop     %rsi                    # restore registers puts used
-        pop     %rdi
+        mov     %rsi, %r12              # argv
 
-        add     $8, %rsi                # point to next argument
-        dec     %rdi                    # count down
-        jnz     main                    # if not done counting keep going
+# We will use ecx to count down form the exponent to zero, esi to hold the
+# value of the base, and eax to hold the running product.
+
+        mov     16(%r12), %rdi          # argv[2]
+        call    atoi                    # y in eax
+        cmp     $0, %eax                # disallow negative exponents
+        jl      error2
+        mov     %eax, %r13d             # y in r13d
+
+        mov     8(%r12), %rdi           # argv
+        call    atoi                    # x in eax
+        mov     %eax, %r14d             # x in r14d
+
+        mov     $1, %eax                # start with answer = 1
+check:
+        test    %r13d, %r13d            # we're counting y downto 0
+        jz      gotit                   # done
+        imul    %r14d, %eax             # multiply in another x
+        dec     %r13d
+        jmp     check
+gotit:                                  # print report on success
+        mov     $answer, %rdi
+        movslq  %eax, %rsi
+        xor     %rax, %rax
+        call    printf
+        jmp     done
+error1:                                 # print error message
+        mov     $badArgumentCount, %edi
+        call    puts
+        jmp     done
+error2:                                 # print error message
+        mov     $negativeExponent, %edi
+        call    puts
+done:                                   # restore saved registers
+        pop     %r14
+        pop     %r13
+        pop     %r12
         ret
 
-format:
-        .asciz  "%s\n"
+answer:
+        .asciz  "%d\n"
+badArgumentCount:
+        .asciz  "Requires exactly two arguments\n"
+negativeExponent:
+        .asciz  "The exponent may not be negative\n"
